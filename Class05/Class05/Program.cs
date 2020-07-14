96d2b9df946f9753c715ad483f508bf692aa2ff5
CimonSector <cimonsector@gmail.com>
2020-07-14 17:05:02 +0900

    project creation

---
+++ Class05/Class05/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Class05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
